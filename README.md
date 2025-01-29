698339452c729f9d4d2f127dc4866575623b3355
marmooo <marmooo@users.noreply.github.com>
2025-01-29 09:53:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# js-timer-benchmark
+
+Benchmark the accuracy of various timers available in JavaScript.
+
+## Result (Chrome)
+
+- foreground 1st run
+
+| method                | 1sec error | 2sec error |
+| --------------------- | ---------: | ---------: |
+| setTimeout            |      0.9ms |      0.6ms |
+| setInterval (10)      |      0.8ms |      0.4ms |
+| setInterval (100)     |      0.6ms |      0.4ms |
+| AudioBufferSourceNode |    106.1ms |      2.2ms |
+| OscillatorNode        |      6.2ms |     14.3ms |
+| ConstantSourceNode    |      6.6ms |     20.2ms |
+| requestAnimationFrame |      9.7ms |      8.8ms |
+
+- foreground
+
+| method                | 1sec error | 2sec error |
+| --------------------- | ---------: | ---------: |
+| setTimeout            |      0.7ms |      0.4ms |
+| setInterval (10)      |      0.6ms |      0.4ms |
+| setInterval (100)     |      0.4ms |      0.5ms |
+| AudioBufferSourceNode |      8.6ms |      5.8ms |
+| OscillatorNode        |      9.3ms |     19.1ms |
+| ConstantSourceNode    |     10.1ms |     20.4ms |
+| requestAnimationFrame |      1.0ms |      0.1ms |
+
+- background 1st run
+
+| method                | 1sec error | 2sec error |
+| --------------------- | ---------: | ---------: |
+| setTimeout            |    989.9ms |    900.6ms |
+| setInterval (10)      |      0.5ms |    999.7ms |
+| setInterval (100)     |    998.8ms |    999.5ms |
+| AudioBufferSourceNode |     89.9ms |      5.8ms |
+| OscillatorNode        |     10.7ms |     19.9ms |
+| ConstantSourceNode    |      3.9ms |     20.2ms |
+| requestAnimationFrame |  34985.8ms |  30565.4ms |
+
+- background
+
+| method                | 1sec error | 2sec error |
+| --------------------- | ---------: | ---------: |
+| setTimeout            |    998.4ms |    390.6ms |
+| setInterval (10)      |   1000.2ms |      0.4ms |
+| setInterval (100)     |    998.8ms |    999.1ms |
+| AudioBufferSourceNode |      8.0ms |      1.3ms |
+| OscillatorNode        |      9.7ms |     19.6ms |
+| ConstantSourceNode    |     10.1ms |     19.5ms |
+| requestAnimationFrame |  14580.2ms |   2245.5ms |
+
+## Conclusion
+
+### setTimeout & setInterval
+
+Suitable for general use. Less accurate in inactive tabs (ref:
+[MDN](https://developer.mozilla.org/en-US/docs/Web/API/Window/setTimeout)).
+
+### requestAnimationFrame
+
+Should run in active tabs.
+
+### AudioBufferSourceNode
+
+It has lower accurary at initial startup than OscillatorNode and
+ConstantSourceNode. However, from the second time onwards, the accuracy is
+higher than theirs.
+
+### OscillatorNode & ConstantSourceNode
+
+Suitable for audio playback or when you need a high-precision timer in inactive
+tabs. It is less accurate than setTimeout and setInterval in active tabs.
+
+## Build
+
+```
+bash build.sh
+```
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,137 @@
+function loadConfig() {
+  if (localStorage.getItem("darkMode") == 1) {
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+  }
+}
+
+function toggleDarkMode() {
+  if (localStorage.getItem("darkMode") == 1) {
+    localStorage.setItem("darkMode", 0);
+    document.documentElement.setAttribute("data-bs-theme", "light");
+  } else {
+    localStorage.setItem("darkMode", 1);
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+  }
+}
+
+function testSetTimeout(delay) {
+  return new Promise((resolve) => {
+    const startTime = performance.now();
+    setTimeout(() => {
+      const endTime = performance.now();
+      const result = (endTime - startTime - delay).toFixed(1);
+      resolve(result);
+    }, delay);
+  });
+}
+
+function testSetInterval(delay, interval) {
+  return new Promise((resolve) => {
+    const startTime = performance.now();
+    const intervalId = setInterval(() => {
+      const now = performance.now();
+      if (now - startTime >= delay) {
+        clearInterval(intervalId);
+        const result = (now - startTime - delay).toFixed(1);
+        resolve(result);
+      }
+    }, interval);
+  });
+}
+
+function testAudioBufferSourceNode(delay) {
+  return new Promise((resolve) => {
+    // const source = audioContext.createBufferSource();
+    const source = new AudioBufferSourceNode(audioContext);
+    const startTime = performance.now();
+    source.onended = () => {
+      const endTime = performance.now();
+      const result = (endTime - startTime - delay).toFixed(1);
+      resolve(result);
+    };
+    const time = audioContext.currentTime + delay / 1000;
+    source.start(time);
+    source.stop(time);
+  });
+}
+
+function testConstantSourceNode(delay) {
+  return new Promise((resolve) => {
+    // const source = audioContext.createConstantSource();
+    const source = new ConstantSourceNode(audioContext);
+    const startTime = performance.now();
+    source.onended = () => {
+      const endTime = performance.now();
+      const result = (endTime - startTime - delay).toFixed(1);
+      resolve(result);
+    };
+    const time = audioContext.currentTime + delay / 1000;
+    source.start(time);
+    source.stop(time);
+  });
+}
+
+function testOscillatorNode(delay) {
+  return new Promise((resolve) => {
+    // const oscillator = audioContext.createOscillator();
+    const oscillator = new OscillatorNode(audioContext);
+    const startTime = performance.now();
+    oscillator.onended = () => {
+      const endTime = performance.now();
+      const result = (endTime - startTime - delay).toFixed(1);
+      resolve(result);
+    };
+    const time = audioContext.currentTime + delay / 1000;
+    oscillator.start(time);
+    oscillator.stop(time);
+  });
+}
+
+function testRequestAnimationFrame(delay) {
+  return new Promise((resolve) => {
+    const startTime = performance.now();
+    function frameCallback() {
+      const now = performance.now();
+      if (now - startTime >= delay) {
+        const result = (now - startTime - delay).toFixed(1);
+        resolve(result);
+      } else {
+        requestAnimationFrame(frameCallback);
+      }
+    }
+    requestAnimationFrame(frameCallback);
+  });
+}
+
+function test() {
+  const trs = document.getElementById("result").querySelectorAll("tr");
+  setTimeout(async () => {
+    for (let i = 1; i < trs.length; i++) {
+      trs[i].children[1].textContent = "";
+      trs[i].children[2].textContent = "";
+    }
+    const delays = [1000, 2000];
+    for (let i = 0; i < delays.length; i++) {
+      const delay = delays[i];
+      trs[1].children[i + 1].textContent = await testSetTimeout(delay) + "ms";
+      trs[2].children[i + 1].textContent = await testSetInterval(delay, 10) +
+        "ms";
+      trs[3].children[i + 1].textContent = await testSetInterval(delay, 100) +
+        "ms";
+      trs[4].children[i + 1].textContent =
+        await testAudioBufferSourceNode(delay) + "ms";
+      trs[5].children[i + 1].textContent = await testConstantSourceNode(delay) +
+        "ms";
+      trs[6].children[i + 1].textContent = await testOscillatorNode(delay) +
+        "ms";
+      trs[7].children[i + 1].textContent =
+        await testRequestAnimationFrame(delay) + "ms";
+    }
+  }, 1000);
+}
+
+loadConfig();
+const audioContext = new AudioContext();
+
+document.getElementById("toggleDarkMode").onclick = toggleDarkMode;
+document.getElementById("startButton").onclick = test;
